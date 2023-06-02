12d08e43e78b3505276498fa5fc151255f29e5c9
pvfonseca <paulovictordafonseca@gmail.com>
2023-06-01 22:48:59 -0300

    updt

--- src/notebooks/mqe_aula11.jl
+++ src/notebooks/mqe_aula11.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.9
+# v0.19.26
 
 using Markdown
 using InteractiveUtils
@@ -393,6 +393,131 @@ md"
 > **Exercício.** Mostre que $f(x,y) = xy$ definida no $\mathbb{R}_+^2$ é quase-côncava
 "
 
+# ╔═╡ 74fe5d05-95e1-40f8-b4f3-48245094defb
+md"""
+!!! warning "Determinante aumentado x Hessiano aumentado"
+	* Lembrando que, anteriormente, vimos que o Hessiano aumentado associado ao problema de otimização com restrição de igualdade é dado por:
+
+	$$|\bar{H}| = \begin{vmatrix}0 & g_1 & g_2 & \dots & g_n \\
+		g_1 & \mathcal{L}_{11} & \mathcal{L}_{12} & \dots & \mathcal{L}_{1n} \\
+		g_2 & \mathcal{L}_{21} & \mathcal{L}_{22} & \dots & \mathcal{L}_{2n} \\
+		\vdots & \vdots & \vdots & \ddots & \vdots \\
+		g_n & \mathcal{L}_{n1} & \mathcal{L}_{n2} & \dots & \mathcal{L}_{nn}\end{vmatrix}$$
+	
+	* Note, portanto, que o determinante aumentado $|B|$ que definimos acima difere do Hessiano aumentado $|\bar{H}|$ em duas maneiras:
+	    1. Os elementos acrescidos em $|B|$ são as derivadas parciais de primeira ordem da função $f$, e não da função $g$
+	    2. Os elementos restantes em $|B|$ são as derivadas parciais de segunda ordem da função $f$, ao invés de estarem associados à função Lagrangeana $\mathcal{L}$
+"""
+
+# ╔═╡ 877bb8bc-b3e1-4807-9f1c-1774f6671b2f
+md"
+* Considere, no entanto, o caso especial de uma equação linear de restrição:
+
+$$g(x_1, \dots, x_n) = \sum_{j=1}^n a_j x_j = c$$
+
+* Neste caso, a função Lagrangeana será dada por:
+
+$$\mathcal{L} = f(x_1, \dots, x_n) + \lambda\left(c - \sum_{j=1}^n a_jx_j\right)$$
+
+* De modo que:
+
+$$\mathcal{L}_i = f_i - \lambda a_i \Rightarrow \mathcal{L}_{ij} = f_{ij}, \qquad i, j \in \{1, \dots, n\}$$
+
+* Examinando os elementos acrescidos ao determinante aumentado, notamos que a função linear de restrição resulta em derivadas primeiras dadas por: $g_i = a_i$
+
+* Além disso, quando as condições necessárias de primeira ordem são satisfeitas temos que:
+
+$$\mathcal{L}_i = f_i - \lambda a_i = 0 \Rightarrow f_i = \lambda a_i = \lambda g_i$$
+
+* Portanto, temos o seguinte resultado:
+
+$$|B| = \begin{vmatrix} 0 & \lambda g_1 & \lambda g_2 & \dots & \lambda g_n \\ \lambda g_1 & \mathcal{L}_{11} & \mathcal{L}_{12} & \dots & \mathcal{L}_{1n} \\ \lambda g_2 & \mathcal{L}_{21} & \mathcal{L}_{22} & \dots & \mathcal{L}_{2n} \\ \vdots & \vdots & \vdots & \ddots & \vdots \\ \lambda g_n & \mathcal{L}_{n1} & \mathcal{L}_{n2} & \dots & \mathcal{L}_{nn}\end{vmatrix} = \lambda^2 \begin{vmatrix} 0 & g_1 &  g_2 & \dots &  g_n \\  g_1 & \mathcal{L}_{11} & \mathcal{L}_{12} & \dots & \mathcal{L}_{1n} \\  g_2 & \mathcal{L}_{21} & \mathcal{L}_{22} & \dots & \mathcal{L}_{2n} \\ \vdots & \vdots & \vdots & \ddots & \vdots \\  g_n & \mathcal{L}_{n1} & \mathcal{L}_{n2} & \dots & \mathcal{L}_{nn}\end{vmatrix}$$
+
+* Temos, então, que para o caso de restrições lineares o seguinte resultado é válido:
+
+$$|B| = \lambda^2 |\bar{H}|$$
+
+* Consequentemente, no caso de restrições lineares, **os dois determinantes aumentados sempre possuem o mesmo sinal algébrico quando avaliados no ponto estacionário de $\mathcal{L}$**
+
+* Pelo mesmo critério, os menores principais líderes $|B_i|$ e $\bar{H}_i$ ($i \in \{1, \dots, n\}$) também devem compartilhar o mesmo sinal naquele ponto
+"
+
+# ╔═╡ 1b329446-7e98-473d-b07d-8a37a43f6351
+md"""
+!!! info "Quase-concavidade e condições de segunda ordem"
+	Portanto, se o determinante aumentado $|B|$ satisfizer a condição suficiente para quase-concavidade estrita, o Hessiano aumentado $|\bar{H}|$ deve, então, satisfazer a condição suficiente de segunda ordem para o problema de maximização restrita
+
+	Lembrando que a condição suficiente de segunda ordem para um máximo restrito é: $|\bar{H}_1| < 0, |\bar{H}_2| > 0, \dots, (-1)^n|\bar{H}_n|>0$
+"""
+
+# ╔═╡ 4dbdb8be-db39-4ef0-8b60-6deb170f9336
+md"""
+Quase-concavidade e condições de segunda ordem. Fonte: Chiang e Wainwright (2006)
+$(Resource("https://raw.githubusercontent.com/gist/fonsp/9a36c183e2cad7c8fc30290ec95eb104/raw/ca3a38a61f95cd58d79d00b663a3c114d21e284e/cute.svg"))
+"""
+
+# ╔═╡ 6bc3325e-90e7-4113-9c00-d6b9d8fc6939
+md"""
+!!! warning "Quase-convexidade"
+	O diagrama acima pode ser adaptado para o caso de quase-convexidade
+"""
+
+# ╔═╡ 377e653d-45c1-488b-91d7-862de8917e0f
+md"
+* Parte esquerda do diagrama:
+$$\left.\begin{array}{l} \text{- $x^*$ é um extremo relativo restrito} \\ \text{- $f$ explicitamente quase-côncava} \\ \text{- conjunto restrição convexo}\end{array}\right\} \Rightarrow \text{$x^*$ é um máximo absoluto restrito}$$
+
+"
+
+# ╔═╡ 8f85106e-ebd8-4900-862e-582bba8ad9b0
+md"""
+!!! correct "Função explicitamente quase-côncava"
+	Uma função $f$ quase-côncava é **explicitamente quase-côncava** se satisfizer a seguinte propriedade adicional:
+
+	$$f(x_2) > f(x_1) \Rightarrow f\left(\lambda x_1 + (1-\lambda)x_2\right) > f(x_1)$$
+
+	* Sempre que um determinado ponto sobre a superfície da função, $f(x_2)$, for mais alto que um outro, $f(x_1)$, então, todos os pontos intermediários também devem ser mais altos que $f(x_1)$
+
+	* Essa condição exclui quaisquer segmentos de planos horizontais sobre a superfície da função, exceto um possível platô no topo
+"""
+
+# ╔═╡ 72dfd952-7905-49f2-a35f-83cc29bc4fcb
+md"
+* Note que a condição para quase-concavidade explícita é mais fraca que a condição para quase-concavidade estrita
+
+* Quase-concavidade estrita requer que $f\left(\lambda x_1 + (1-\lambda)x_2\right) > f(x_1)$, mesmo quando $f(x_1) = f(x_2)$, implicando que segmentos de planos não horizontais também não são permitidos
+"
+
+# ╔═╡ ae925b24-ed95-42e9-bd2a-a0cd64294ab8
+md"""
+!!! danger "Máximo absoluto restrito"
+	* Quando $f$ é uma função explicitamente quase-côncava e o conjunto restrição é convexo, então, o valor estacionário encontrado nas CPOs do problema restrito será um máximo absoluto restrito
+
+	* Portanto, o máximo local encontrado sobre o subconjunto convexo da superfície será um máximo global restrito. Neste caso, estamos tratando com a porção de uma superfície (ou hipersuperfície) em formato de sino, livre de segmentos horizontais, que está diretamente acima de um conjunto convexo estabelecido no domínio
+"""
+
+# ╔═╡ f6304c43-9d2b-4a58-9794-00442971f6b2
+md"""
+!!! danger "Unicidade do máximo absoluto restrito"
+	* A porção direita do diagrama envolve a condição mais forte de quase-concavidade estrita
+	* Uma função estritamente quase-côncava deve ser explicitamente quase-côncava, embora a recíproca não seja verdadeira
+	* Portanto, quando quase-concavidade estrita substitui a quase-concavidade explícita, ainda assim, a condição de um máximo absoluto restrito está assegurada
+	* Mas, desta vez, o máximo absoluto restrito será, também, **único**, visto que a ausência de qualquer segmento plano em qualquer parte sobre a superfície exclui, definitivamente, a possibilidade de vários máximos restritos
+"""
+
+# ╔═╡ 77cc00b7-e2a7-4f15-8a35-cb9682a8b33b
+md"
+> **Exercício.** Considere o seguinte problema primal de maximização de utilidade do consumidor:
+>
+> $$\begin{align}\max_{x,y} \quad & \quad Ax^\alpha y^\beta, \qquad x, y > 0, \quad 0<\alpha, \beta < 1\\ \text{s.r.} \quad & \quad p_x x + p_y y = I\end{align}$$
+>
+> 1. Mostre que a função utilidade deste consumidor é estritamente quase-côncava
+>
+> 2. Mostre que a reta orçamentária forma um conjunto convexo
+>
+> 3. O que podemos concluir sobre o problema primal de maximização de utilidade sujeito à restrição orçamentária?
+"
+
 # ╔═╡ 0329b1e6-7e40-4f86-b3a2-a30875af12a2
 md"
 ## 📚 Bibliografia
@@ -428,9 +553,9 @@ PlutoUI = "~0.7.51"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.8.5"
+julia_version = "1.9.0"
 manifest_format = "2.0"
-project_hash = "e9be83a6c7fd21f04cfc4051871fb477d5cc6cf9"
+project_hash = "4ef839c502a3f45a4cd42573219646d9b1a97b15"
 
 [[deps.AbstractPlutoDingetjes]]
 deps = ["Pkg"]
@@ -508,15 +633,19 @@ uuid = "5ae59095-9a9b-59fe-a467-6f913c188581"
 version = "0.12.10"
 
 [[deps.Compat]]
-deps = ["Dates", "LinearAlgebra", "UUIDs"]
+deps = ["UUIDs"]
 git-tree-sha1 = "7a60c856b9fa189eb34f5f8a6f6b5529b7942957"
 uuid = "34da2185-b29b-5c13-b0c7-acf172513d20"
 version = "4.6.1"
+weakdeps = ["Dates", "LinearAlgebra"]
+
+    [deps.Compat.extensions]
+    CompatLinearAlgebraExt = "LinearAlgebra"
 
 [[deps.CompilerSupportLibraries_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
-version = "1.0.1+0"
+version = "1.0.2+0"
 
 [[deps.ConcurrentUtilities]]
 deps = ["Serialization", "Sockets"]
@@ -529,6 +658,14 @@ deps = ["LinearAlgebra"]
 git-tree-sha1 = "738fec4d684a9a6ee9598a8bfee305b26831f28c"
 uuid = "187b0558-2788-49d3-abe0-74a17ed4e7c9"
 version = "1.5.2"
+
+    [deps.ConstructionBase.extensions]
+    ConstructionBaseIntervalSetsExt = "IntervalSets"
+    ConstructionBaseStaticArraysExt = "StaticArrays"
+
+    [deps.ConstructionBase.weakdeps]
+    IntervalSets = "8197267c-284f-5f27-9208-e0e47529a953"
+    StaticArrays = "90137ffa-7385-5640-81b9-e52037218182"
 
 [[deps.Contour]]
 git-tree-sha1 = "d05d9e7b7aedff4e5b51a029dced05cfb6125781"
@@ -552,7 +689,9 @@ uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
 
 [[deps.DelimitedFiles]]
 deps = ["Mmap"]
+git-tree-sha1 = "9e2f36d3c96a820c678f2f1f1782582fcf685bae"
 uuid = "8bb1440f-4735-579b-a4ab-409b98df4dab"
+version = "1.9.1"
 
 [[deps.DocStringExtensions]]
 deps = ["LibGit2"]
@@ -755,6 +894,14 @@ git-tree-sha1 = "099e356f267354f46ba65087981a77da23a279b7"
 uuid = "23fbe1c1-3f47-55db-b15f-69d7ec21a316"
 version = "0.16.0"
 
+    [deps.Latexify.extensions]
+    DataFramesExt = "DataFrames"
+    SymEngineExt = "SymEngine"
+
+    [deps.Latexify.weakdeps]
+    DataFrames = "a93c6f00-e57d-5684-b7b6-d8193f3e46c0"
+    SymEngine = "123dc426-2d89-5057-bbad-38513e3affd8"
+
 [[deps.LibCURL]]
 deps = ["LibCURL_jll", "MozillaCACerts_jll"]
 uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
@@ -826,14 +973,20 @@ uuid = "38a345b3-de98-5d2b-a5d3-14cd9215e700"
 version = "2.36.0+0"
 
 [[deps.LinearAlgebra]]
-deps = ["Libdl", "libblastrampoline_jll"]
+deps = ["Libdl", "OpenBLAS_jll", "libblastrampoline_jll"]
 uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
 
 [[deps.LogExpFunctions]]
-deps = ["ChainRulesCore", "ChangesOfVariables", "DocStringExtensions", "InverseFunctions", "IrrationalConstants", "LinearAlgebra"]
+deps = ["DocStringExtensions", "IrrationalConstants", "LinearAlgebra"]
 git-tree-sha1 = "0a1b7c2863e44523180fdb3146534e265a91870b"
 uuid = "2ab3a3ac-af41-5b50-aa03-7779005ae688"
 version = "0.3.23"
+weakdeps = ["ChainRulesCore", "ChangesOfVariables", "InverseFunctions"]
+
+    [deps.LogExpFunctions.extensions]
+    LogExpFunctionsChainRulesCoreExt = "ChainRulesCore"
+    LogExpFunctionsChangesOfVariablesExt = "ChangesOfVariables"
+    LogExpFunctionsInverseFunctionsExt = "InverseFunctions"
 
 [[deps.Logging]]
 uuid = "56ddb016-857b-54e1-b83d-db4d58db5568"
@@ -868,7 +1021,7 @@ version = "1.1.7"
 [[deps.MbedTLS_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
-version = "2.28.0+0"
+version = "2.28.2+0"
 
 [[deps.Measures]]
 git-tree-sha1 = "c13304c81eec1ed3af7fc20e75fb6b26092a1102"
@@ -886,7 +1039,7 @@ uuid = "a63ad114-7e13-5084-954f-fe012c677804"
 
 [[deps.MozillaCACerts_jll]]
 uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
-version = "2022.2.1"
+version = "2022.10.11"
 
 [[deps.NaNMath]]
 deps = ["OpenLibm_jll"]
@@ -907,7 +1060,7 @@ version = "1.3.5+1"
 [[deps.OpenBLAS_jll]]
 deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
 uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
-version = "0.3.20+0"
+version = "0.3.21+4"
 
 [[deps.OpenLibm_jll]]
 deps = ["Artifacts", "Libdl"]
@@ -946,7 +1099,7 @@ version = "1.6.0"
 [[deps.PCRE2_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "efcefdf7-47ab-520b-bdef-62a2eaa19f15"
-version = "10.40.0+0"
+version = "10.42.0+0"
 
 [[deps.Parsers]]
 deps = ["Dates", "PrecompileTools", "UUIDs"]
@@ -966,9 +1119,9 @@ uuid = "30392449-352a-5448-841d-b1acce4e97dc"
 version = "0.40.1+0"
 
 [[deps.Pkg]]
-deps = ["Artifacts", "Dates", "Downloads", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
+deps = ["Artifacts", "Dates", "Downloads", "FileWatching", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
 uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
-version = "1.8.0"
+version = "1.9.0"
 
 [[deps.PlotThemes]]
 deps = ["PlotUtils", "Statistics"]
@@ -987,6 +1140,20 @@ deps = ["Base64", "Contour", "Dates", "Downloads", "FFMPEG", "FixedPointNumbers"
 git-tree-sha1 = "ad59edfb711a4751e0b8271454df47f84a47a29e"
 uuid = "91a5bcdd-55d7-5caf-9e0b-520d859cae80"
 version = "1.38.14"
+
+    [deps.Plots.extensions]
+    FileIOExt = "FileIO"
+    GeometryBasicsExt = "GeometryBasics"
+    IJuliaExt = "IJulia"
+    ImageInTerminalExt = "ImageInTerminal"
+    UnitfulExt = "Unitful"
+
+    [deps.Plots.weakdeps]
+    FileIO = "5789e2e9-d7fb-5bc7-8068-2c6fae9b9549"
+    GeometryBasics = "5c1252a2-5f33-56bf-86c9-59e7332b4326"
+    IJulia = "7073ff75-c697-5162-941a-fcdaad2a7d2a"
+    ImageInTerminal = "d8c32880-2388-543b-8c61-d9f865259254"
+    Unitful = "1986cc42-f94f-5a68-af5c-568840ba703d"
 
 [[deps.PlutoUI]]
 deps = ["AbstractPlutoDingetjes", "Base64", "ColorTypes", "Dates", "FixedPointNumbers", "Hyperscript", "HypertextLiteral", "IOCapture", "InteractiveUtils", "JSON", "Logging", "MIMEs", "Markdown", "Random", "Reexport", "URIs", "UUIDs"]
@@ -1087,18 +1254,23 @@ uuid = "a2af1166-a08f-5f64-846c-94a0d3cef48c"
 version = "1.1.0"
 
 [[deps.SparseArrays]]
-deps = ["LinearAlgebra", "Random"]
+deps = ["Libdl", "LinearAlgebra", "Random", "Serialization", "SuiteSparse_jll"]
 uuid = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
 
 [[deps.SpecialFunctions]]
-deps = ["ChainRulesCore", "IrrationalConstants", "LogExpFunctions", "OpenLibm_jll", "OpenSpecFun_jll"]
+deps = ["IrrationalConstants", "LogExpFunctions", "OpenLibm_jll", "OpenSpecFun_jll"]
 git-tree-sha1 = "ef28127915f4229c971eb43f3fc075dd3fe91880"
 uuid = "276daf66-3868-5448-9aa4-cd146d93841b"
 version = "2.2.0"
+weakdeps = ["ChainRulesCore"]
+
+    [deps.SpecialFunctions.extensions]
+    SpecialFunctionsChainRulesCoreExt = "ChainRulesCore"
 
 [[deps.Statistics]]
 deps = ["LinearAlgebra", "SparseArrays"]
 uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
+version = "1.9.0"
 
 [[deps.StatsAPI]]
 deps = ["LinearAlgebra"]
@@ -1112,15 +1284,20 @@ git-tree-sha1 = "75ebe04c5bed70b91614d684259b661c9e6274a4"
 uuid = "2913bbd2-ae8a-5f71-8c99-4fb6c76f3a91"
 version = "0.34.0"
 
+[[deps.SuiteSparse_jll]]
+deps = ["Artifacts", "Libdl", "Pkg", "libblastrampoline_jll"]
+uuid = "bea87d4a-7f5b-5778-9afe-8cc45184846c"
+version = "5.10.1+6"
+
 [[deps.TOML]]
 deps = ["Dates"]
 uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
-version = "1.0.0"
+version = "1.0.3"
 
 [[deps.Tar]]
 deps = ["ArgTools", "SHA"]
 uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
-version = "1.10.1"
+version = "1.10.0"
 
 [[deps.TensorCore]]
 deps = ["LinearAlgebra"]
@@ -1166,6 +1343,10 @@ deps = ["ConstructionBase", "Dates", "LinearAlgebra", "Random"]
 git-tree-sha1 = "ba4aa36b2d5c98d6ed1f149da916b3ba46527b2b"
 uuid = "1986cc42-f94f-5a68-af5c-568840ba703d"
 version = "1.14.0"
+weakdeps = ["InverseFunctions"]
+
+    [deps.Unitful.extensions]
+    InverseFunctionsUnitfulExt = "InverseFunctions"
 
 [[deps.UnitfulLatexify]]
 deps = ["LaTeXStrings", "Latexify", "Unitful"]
@@ -1331,7 +1512,7 @@ version = "1.4.0+3"
 [[deps.Zlib_jll]]
 deps = ["Libdl"]
 uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
-version = "1.2.12+3"
+version = "1.2.13+0"
 
 [[deps.Zstd_jll]]
 deps = ["Artifacts", "JLLWrappers", "Libdl"]
@@ -1358,9 +1539,9 @@ uuid = "0ac62f75-1d6f-5e53-bd7c-93b484bb37c0"
 version = "0.15.1+0"
 
 [[deps.libblastrampoline_jll]]
-deps = ["Artifacts", "Libdl", "OpenBLAS_jll"]
+deps = ["Artifacts", "Libdl"]
 uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
-version = "5.1.1+0"
+version = "5.7.0+0"
 
 [[deps.libfdk_aac_jll]]
 deps = ["Artifacts", "JLLWrappers", "Libdl", "Pkg"]
@@ -1462,6 +1643,17 @@ version = "1.4.1+0"
 # ╟─e0b3b61f-f6fa-43df-a756-5442f0893a81
 # ╟─eb68c85f-f21c-4f46-8e09-f5beca37f111
 # ╟─37995ce7-c044-43dc-91bf-79a511a0366d
+# ╟─74fe5d05-95e1-40f8-b4f3-48245094defb
+# ╟─877bb8bc-b3e1-4807-9f1c-1774f6671b2f
+# ╟─1b329446-7e98-473d-b07d-8a37a43f6351
+# ╟─4dbdb8be-db39-4ef0-8b60-6deb170f9336
+# ╟─6bc3325e-90e7-4113-9c00-d6b9d8fc6939
+# ╟─377e653d-45c1-488b-91d7-862de8917e0f
+# ╟─8f85106e-ebd8-4900-862e-582bba8ad9b0
+# ╟─72dfd952-7905-49f2-a35f-83cc29bc4fcb
+# ╟─ae925b24-ed95-42e9-bd2a-a0cd64294ab8
+# ╟─f6304c43-9d2b-4a58-9794-00442971f6b2
+# ╟─77cc00b7-e2a7-4f15-8a35-cb9682a8b33b
 # ╟─0329b1e6-7e40-4f86-b3a2-a30875af12a2
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
